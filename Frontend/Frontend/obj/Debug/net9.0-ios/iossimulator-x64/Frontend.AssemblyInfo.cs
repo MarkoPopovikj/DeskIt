53bd83147d6ad2330ad1da995cb3e2f8902816e0
MarkoPopovikj <popovicmarko1343@gmail.com>
2025-06-30 18:11:18 +0200

    docker first initialized

--- Frontend/Frontend/obj/Debug/net9.0-ios/iossimulator-x64/Frontend.AssemblyInfo.cs
+++ Frontend/Frontend/obj/Debug/net9.0-ios/iossimulator-x64/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+98f47071272b91b94979afefee6715c28a20598a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
